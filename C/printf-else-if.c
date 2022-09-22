fe96569175b88919614da350a967e4435f7197fa
José M <102778708+baalgx@users.noreply.github.com>
2022-09-22 09:03:24 -0600

    C: add class test
    testing the else, if, scanf, and printf

---
+++ C/printf-else-if.c
@@ -0,0 +1,49 @@
+/* #include<stdio.h>
+int main(){
+    /* Declaración de constantes
+    char x = 's';
+    char z = 'n';
+
+
+    // Declaracion de variables
+    int gustos;
+
+    // Bloque de instrucciones
+    printf("A usted le gusta BTS? \nEscriba 1 para SI, escriba 2 para NO \n");
+    scanf("%i",&gustos);
+    if(gustos==1){
+        printf("usted es re homosexual XD");
+        }
+        else{
+        printf("Usted es chad, siga adelante");
+    }
+        if(gustos>=2){
+        printf("USTED ES ESTUPIDO VALE\n");
+    }
+}
+    */
+
+int main(){
+    /* Declaración de constantes
+    char x = 's';
+    char z = 'n';
+    */
+
+    // Declaracion de variables
+    int gustos;
+
+    // Bloque de instrucciones
+    printf("A usted le gusta BTS? \nEscriba 1 para SI, escriba 2 para NO \n");
+    scanf("%i",&gustos);
+    if(gustos==1){
+        printf("usted es re homosexual XD");
+        }
+        else if(gustos==2){
+        printf("Usted es chad, siga adelante");
+        }
+        else{
+        printf("BRO TE PEDI 1 O 2 NO UN NUMERO RANDOM XDDDDD\n");
+    }
+}
+
+
