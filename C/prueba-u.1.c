ff2212964a9d180409d2e31cfe891b94d2b49179
José M <102778708+baalgx@users.noreply.github.com>
2022-09-23 20:57:06 -0500

    C: Prueba: Uso de while, if, máximo y mínimo
    * Necesita un fix en la parte del while xd

---
+++ C/prueba-u.1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+    // Declaración de variables
+    float est, prom=0, max, min;
+    int i=0;
+
+    //Bloque de instrucciones
+    printf("Estatura: ");
+    scanf("%f",&est);
+    max=est;
+    min=est;
+    while(est==0){ // Mientras est sea =! de 0, se cumple lo otro.
+        if(est>max){ // Si max>est, la nueva est será max
+            max=est;
+        }
+            else if(est<min){ // Si min>est, la nueva est será min
+            min=est;
+        }
+            prom=prom+est; // Esto es un acumulador
+            printf("Ingrese la estatura:\nSi desea finalizar el programa, escriba 0 ");
+            scanf("%f",&est);
+            i++;
+    }
+    prom=(prom/i);
+    printf("El promedio de sus estaturas es de: %f\n", prom);
+    printf("La estatura máxima es de: %f\n", max);
+    printf("La estatura mínima es: %f\n", min);
+}
