865715342e55cbdcc2b7b9add9b8bf0ac687288b
José M <102778708+baalgx@users.noreply.github.com>
2022-09-23 21:11:18 -0500

    C: printf-else-if: Corregir lógica
    * Al cambiar las preguntas, se me olvidó corregir el orden xd.

--- C/printf-else-if.c
+++ C/printf-else-if.c
@@ -12,10 +12,10 @@ int main(){
     printf("A usted le gusta amogus? \nEscriba 1 para SI, escriba 2 para NO.\n");
     scanf("%i",&gustos);
     if(gustos==1){
-        printf("usted no es basado... me ha fallado.");
+        printf("Usted es chad, siga adelante.");
         }
         else if(gustos==2){
-        printf("Usted es chad, siga adelante");
+        printf("Usted no es basado... me ha fallado.");
         }
         else{
         printf("BRO TE PEDI 1 O 2 NO UN NUMERO RANDOM XD\n");
