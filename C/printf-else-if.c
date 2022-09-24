a972794760ee8b22746387a3b89675763ff1ccad
José M <102778708+baalgx@users.noreply.github.com>
2022-09-23 21:08:05 -0500

    C: printf-else-if: Borrar código innecesario
    * Ya en la parte de abajo está el código bien hecho.
    * Cambiar las preguntas, hacerlo más family friendly.

--- C/printf-else-if.c
+++ C/printf-else-if.c
@@ -1,28 +1,4 @@
-/* #include<stdio.h>
-int main(){
-    /* Declaración de constantes
-    char x = 's';
-    char z = 'n';
-
-
-    // Declaracion de variables
-    int gustos;
-
-    // Bloque de instrucciones
-    printf("A usted le gusta BTS? \nEscriba 1 para SI, escriba 2 para NO \n");
-    scanf("%i",&gustos);
-    if(gustos==1){
-        printf("usted es re homosexual XD");
-        }
-        else{
-        printf("Usted es chad, siga adelante");
-    }
-        if(gustos>=2){
-        printf("USTED ES ESTUPIDO VALE\n");
-    }
-}
-    */
-
+#include<stdio.h>
 int main(){
     /* Declaración de constantes
     char x = 's';
@@ -33,17 +9,15 @@ int main(){
     int gustos;
 
     // Bloque de instrucciones
-    printf("A usted le gusta BTS? \nEscriba 1 para SI, escriba 2 para NO \n");
+    printf("A usted le gusta amogus? \nEscriba 1 para SI, escriba 2 para NO.\n");
     scanf("%i",&gustos);
     if(gustos==1){
-        printf("usted es re homosexual XD");
+        printf("usted no es basado... me ha fallado.");
         }
         else if(gustos==2){
         printf("Usted es chad, siga adelante");
         }
         else{
-        printf("BRO TE PEDI 1 O 2 NO UN NUMERO RANDOM XDDDDD\n");
+        printf("BRO TE PEDI 1 O 2 NO UN NUMERO RANDOM XD\n");
     }
 }
-
-
