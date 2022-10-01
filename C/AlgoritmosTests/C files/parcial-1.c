86f2df769915da8949af0e3ed9e351b8e28b518a
José M <102778708+baalgx@users.noreply.github.com>
2022-10-01 15:27:24 -0500

    C: parcial-1: Añadir la altura máxima y mínima
    * También imprimir únicamente dos decimales con %.2f

--- C/AlgoritmosTests/C files/parcial-1.c
+++ C/AlgoritmosTests/C files/parcial-1.c
@@ -8,14 +8,24 @@ int main(){
     printf("Introduzca 1 para continuar, introduzca 0 para salir. ");
     scanf("%i",&x);
     cont=0;
+    max=estatura;
+    min=estatura;
     while(x==1){
         printf("Introduzca la estatura de los miembros: ");
         scanf("%f",&estatura);
+            if(estatura>max){
+                max=estatura;
+            }
+            else{
+                min=estatura;
+            }
         z=estatura+z;
         cont=cont+1;
         printf("Escriba 1 para continuar, 0 para salir. ");
         scanf("%i",&x);
         }
     prom=z/cont;
-    printf("Su estatura promedio es: %f",prom);
+    printf("Su estatura promedio es: %.2f \n",prom);
+    printf("La altura máxima es de: %.2f \n",max);
+    printf("La altura mínima es de: %.2f \n",min);
 }
