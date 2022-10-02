4332d06825914b6df70a4513939ba561144e2952
José M <102778708+baalgx@users.noreply.github.com>
2022-10-02 02:17:42 -0500

    C: parcial-1: Cambios (nuevamente)
    * También especificar que solo se puede usar el while.

--- C/AlgoritmosTests/C files/parcial-1.c
+++ C/AlgoritmosTests/C files/parcial-1.c
@@ -3,16 +3,16 @@ Escriba un algoritmo que permita leer N cantidad de estaturas de un grupo de per
 Se requiere que el algoritmo imprima el promedio de todas las estaturas.
 Además de la estatura más alta y más baja.
 Las iteraciones deben ser controladas por el usuario, no se permite preguntar la cantidad de iteraciones al inicio.
+Solamente podrá utilizar la estructura while.
 */
 
 #include<stdio.h>
 // Declaración de variables
 float estatura, prom, max, min, z;
 int x, cont;
-
 // Bloque de instrucciones
 int main(){
-    printf("Introduzca 1 para continuar, introduzca 0 para salir. ");
+    printf("Introduzca 1 para inicializar el programa o introduzca 0 para salir. ");
     scanf("%i",&x);
     cont=0;
     max=0;
@@ -23,12 +23,12 @@ int main(){
         if(estatura>max){
             max=estatura;
         }
-        else if(estatura<min){
+        if(estatura<min){
             min=estatura;
         }
         z=estatura+z;
         cont=cont+1;
-        printf("Escriba 1 para continuar, 0 para salir. ");
+        printf("Introduzca 1 para continuar, 0 para salir. ");
         scanf("%i",&x);
         }
     prom=z/cont;
