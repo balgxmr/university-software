afc4d270008d1af91341ec1b9cef7278773d8856
balgxmr <bal.prxgm1407@gmail.com>
2022-11-28 01:21:33 -0500

    C: arreglos-p1, ok
    divertido eh

---
+++ C/AlgoritmosTests/C files/arreglos-p1.c
@@ -0,0 +1,22 @@
+#include<string.h>
+#include<stdio.h>
+int main(){
+    int i;
+    char nombres[6][10] = {"Jairo", "Angelica", "Mariana", "Jose", "Pedro", "Juan"};
+    char alum[10];
+    int cont=0;
+    printf("aaa \n");
+    scanf("%s",alum);
+    for (i=0;i<6;i++){
+        if(strcmp(nombres[i], alum)==0){
+            printf("El estudiante %s si está dentro del listado.",nombres[i]);
+        }
+        else{
+            cont++;
+            }
+    }
+    if(cont==6){
+        printf("El estudiante %s no se encuentra dentro del listado.", alum);
+    }
+return 0;
+}
