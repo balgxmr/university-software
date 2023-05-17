b93e7dc7cc0ed7e128e8d1885cd872f1f82ecfe2
balgxmr <jose@pixelos.net>
2023-05-17 15:31:24 -0500

    Java: Reto: Handle EXIT on main menu

--- Java/Pruebas-Uni/RETO.java
+++ Java/Pruebas-Uni/RETO.java
@@ -7,15 +7,21 @@ public class RETO {
         BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
         boolean loop1 = true;
 
-        while(loop1){ // INCIO LOOP 1
+        while(loop1){ // INICIO LOOP 1
             boolean MenuExc = false;
             Integer menu=0;
-            try{
-
+            try {
                 System.out.println("Ingrese (1) para calcular el area y perimetro del triangulo");
                 System.out.println("Ingrese (2) para calcular el area y perimetro del circulo");
                 System.out.println("Ingrese (3) para calcular el area y perimetro del cuadrado");
-                menu = Integer.parseInt(lector.readLine());
+                String menuExit = lector.readLine();
+
+                if (menuExit.equals("EXIT")) {
+                    System.out.println("El programa ha finalizado! Que tengas buen dia c:");
+                    loop1 = false;
+                    break;
+                }
+                menu = Integer.parseInt(menuExit);
                 MenuExc = true;
             } catch (Exception E){
                 if(MenuExc == false){
@@ -23,7 +29,7 @@ public class RETO {
                     System.out.println("Ingrese una opcion valida");
                     loop1 = true;
                 }
-            }
+            } // END TRY-CATCH
 
             boolean Elado1 = false;
             boolean Elado2 = false;
@@ -32,7 +38,7 @@ public class RETO {
             String entrada;
 
             boolean loop2 = true;
-            switch(menu){ //INICIO MENU
+            switch(menu){ // INICIO MENU
                 case 1:
                 while(loop2) { //INICIO LOOP 2 
                         try{ //INICIO TRY 
@@ -56,16 +62,16 @@ public class RETO {
                             // Formula de Heron
                             double area = Math.sqrt(valor*((valor-lado1)*(valor-lado2)*(valor-lado3)));
                 
-                            System.out.println("El area del triangulo es " + area);
-                            //ESTE FORMATO PARA LAS IMPRESIONES CON SOLO 2 DECIMALES
-                            System.out.printf("El area del triangulo es %.2f",area);
+                            // System.out.println("El area del triangulo es " + area);
+                            // ESTE FORMATO PARA LAS IMPRESIONES CON SOLO 2 DECIMALES
+                            System.out.printf("El area del triangulo es %.2f" + area);
                             System.out.println();
                             
-                            // salir
+                            // Salir
                             System.out.println("Escribe 'EXIT' para finalizar el programa o ingrese 1 para continuar");
                             entrada = lector.readLine(); // convertir a mayusculas
                             
-                            // Evaluar que valor sea igual a "EXIT" usando equals(), y si es asi, finalizar el programa.
+                            // Evaluar que entrada sea igual a "EXIT" usando equals(), y si es asi, finalizar el programa.
                             if (entrada.equals("EXIT")){
                                 System.out.println("El programa ha finalizado! Que tengas buen dia c:");
                                 loop2 = false;
@@ -138,9 +144,5 @@ public class RETO {
             } // FIN MENU
 
         } //FIN LOOP 1
-
-        
-
-
     } // FIN MAIN
 }
