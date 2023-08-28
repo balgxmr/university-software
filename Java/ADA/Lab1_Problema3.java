049681162daeb3adda84299823420107d55096bf
balgxmr <bal.prxgm1407@gmail.com>
2023-08-28 18:00:15 -0500

    Misc changes
    AAAAAAAAA

--- Java/ADA/Lab1_Problema3.java
+++ Java/ADA/Lab1_Problema3.java
@@ -1,13 +1,14 @@
 import java.io.*;
-import java.lang.Math;
 
 public class Lab1_Problema3 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
 
-        // BufferedReader utilizado para leer el input del usuario
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int c = 0;
 
-        System.out.print("Ingresa un numero entero: ");
+        BufferedReader read = 
+        new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Ingrese el numero: ");
         int n = Integer.parseInt(read.readLine());
 
         if(n < 5 && n > -5 || n < -10){
@@ -15,16 +16,11 @@ public class Lab1_Problema3 {
             System.exit(n);
         }
 
-        int valorSum1 = (int) Math.pow(n, 2);
-        int valorSum2 = (int) n*2;
-        int sum = 0;
-       
-        for(int i = -20; i <= valorSum2; i++){ // Sumatoria i = -20 hasta 2n
-            for(int j = 20; j <= valorSum1; j++){
-                sum = sum + (2*i*j) + (4*j);
+        for (int i = -20; i <= 2 * n; i++) {
+            for (int j = 20; j <= n * n; j++) {
+                c = c + 2 * i * j + 4 * j;
             }
         }
-
-        System.out.println("El resultado de las sumatorias dobles es de: " + sum);
+        System.out.println("El resultado es de: " + c);
     }
 }
