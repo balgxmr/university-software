cf306cb3de4e91be3f1e63573ff21a6dfdb91bad
balgxmr <bal.prxgm1407@gmail.com>
2023-09-08 08:57:30 -0500

    C++: Uni: Añadir encabezados para problemas Taller #1

--- C++/Uni/Algoritmos/Taller1_Prob2-1.cpp
+++ C++/Uni/Algoritmos/Taller1_Prob2-1.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+/*
+ * Tomando como referencia las siguientes secuencias de sumas, analice y 
+ * diseñe una sumatoria y su respectivo algoritmo que la pueda resolver.
+ *                             1+3+5+7+9+11
+*/
+
 int main() {
     for(int i = 1; i <= 6; ++i){
         std::cout << 2*i-1 << '\n';
--- C++/Uni/Algoritmos/Taller1_Prob2-2.cpp
+++ C++/Uni/Algoritmos/Taller1_Prob2-2.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+/*
+ * Tomando como referencia las siguientes secuencias de 
+ * sumas, analice y diseñe una sumatoria y su respectivo 
+ * algoritmo que la pueda resolver.
+ *                  1+4+9+16+253
+*/
+
 int main () {
     for (int i = 1; i <= 5; ++i){
         std::cout << i*i << '\n';
