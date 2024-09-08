74e018c23cbc37f805be05844f8de0ddcff92473
balgxmr <jose@pixelos.net>
2024-09-08 15:17:17 -0500

    html-css/JS: concatenar y booleans

---
+++ html-css/CursoWeb/JS/PrimerCode/js/08-concatenar.js
@@ -0,0 +1,10 @@
+// Concatenacion en JS 
+
+const nombre = 'Jose';
+const email = 'jose@gmail.com';
+
+// Concatenacion 
+console.log("Nombre del usuario: " + nombre + " y su Email es: " + email);
+
+// Template Strings - Las variables van en ${nombre_variable}
+console.log(`Nombre del usuario: ${nombre} y su Email es: ${email}`)
---
+++ html-css/CursoWeb/JS/PrimerCode/js/09-booleanos.js
@@ -0,0 +1,7 @@
+// Booleanos en JS
+
+const booleanoUno = true;
+const booleanoDos = false;
+
+console.log(typeof booleanoUno);
+console.log(booleanoDos);
