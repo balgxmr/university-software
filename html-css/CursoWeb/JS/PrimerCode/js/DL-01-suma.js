02fca0f50a12a45014da096a0bfd5acc2cc3dd41
balgxmr <bal.prxgm1407@gmail.com>
2024-09-09 18:36:28 -0500

    html-css/JS: Suma/resta con prompt

---
+++ html-css/CursoWeb/JS/PrimerCode/js/DL-01-suma.js
@@ -0,0 +1,10 @@
+// Para sumar
+let valorUno;
+let valorDos;
+
+valorUno = prompt("Introduce tu primer numero: ");
+valorDos = prompt("Introduce tu segundo valor: ");
+
+let resultado = Number(valorUno) + Number(valorDos);
+
+alert("Tu resultado es: " + resultado);
